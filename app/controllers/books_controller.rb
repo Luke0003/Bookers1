49ce34c206511f03b328516bbea85f186a77318b
EC2 Default User <ec2-user@ip-172-31-15-106.ap-northeast-1.compute.internal>
2022-08-19 06:42:18 +0000

    [Add]3-12 bookの新規登録機能の作成まで完了

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,32 @@
 class BooksController < ApplicationController
   def index
+    @books = Book.all
+    @book = Book.new
   end
   
+  def new
+  end
+  
+  def create
+    @book = Book.new(book_params)
+    @book.save
+    redirect_to "/books/1" #idの指定方法がわからない
+  end
+  
+  def show
+  end
+  
+  def edit
+  end
+  
+  def update
+  end
+
+  def destroy
+  end
+  
+  private 
+    def book_params
+      params.require(:book).permit(:title, :body)
+    end
 end
